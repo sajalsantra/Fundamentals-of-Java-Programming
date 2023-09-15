772ec814b4cd5653b189c7a3e9ffd97cc88a7f78
Sajal Santra <mr.sajal2552@gmail.com>
2023-09-15 12:17:19 +0530

    Arrays

---
+++ Array/Arrays.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void takeInput(int []arr){
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+    }
+    public static void printArray(int []arr){
+        for(int val : arr) System.out.print(val+" ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array Size: ");
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        System.out.print("Enter Array Element: ");
+        takeInput(arr);
+        System.out.print("The Array is: ");
+        printArray(arr);
+        sc.close();
+    }
+}
