ad51833f97012110a27c547e7d880a3e211bda27
Sajal Santra <mr.sajal2552@gmail.com>
2023-09-15 12:25:16 +0530

    Arrays

--- Array/Arrays.java
+++ Array/Arrays.java
@@ -14,15 +14,22 @@ public class Arrays {
         for(int val : arr) System.out.print(val+" ");
         System.out.println();
     }
+    public static int calLength(int []arr){  //Calculate arr length without length() function
+        int size = 0;
+        for(int val : arr) size++;
+        return size;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Array Size: ");
         int n = sc.nextInt();
-        int []arr = new int[n];
+        int []arr = new int[n]; //Inisilize a new Array 
         System.out.print("Enter Array Element: ");
         takeInput(arr);
         System.out.print("The Array is: ");
         printArray(arr);
         sc.close();
+        int []arr2 = {1, 3, 4, 6, 9, 7, 10, 5}; //Inisilize a new Array
+        System.out.println("Array Size: " + calLength(arr2));
     }
 }
